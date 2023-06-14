6745b3eb22d8f6e68003b1a854a4a8ac3f588cb2
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-06-14 13:22:08 +0800

    Edited logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,10 @@ import Logo from "../Assets/logo2.png";
 import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
-    <div className="flex justify-between h-screen lg:h-[40vh] w-full bg-base-200 px-10 py-10">
+    <div className="flex flex-col lg:flex-row lg:justify-between h-[75vh] lg:h-[40vh] w-full bg-base-200 px-10 py-10">
       <div className="flex w-full h-fit gap-3 lg:gap-10">
         <div className="flex gap-2 flex-col w-1/2 lg:w-fit h-full">
-          <img src={Logo} className="w-1/3 lg:w-7" alt="Logo" />
+          <img src={Logo} className="w-1/4 lg:w-7" alt="Logo" />
           <a
             className="text-sm font-light hover:text-blue-700 transition-colors duration-500"
             href="/"
@@ -87,7 +87,7 @@ function Footer() {
         </div>
       </div>
       <div>
-        <div className="flex mt-3 lg:mt-10 gap-3">
+        <div className="flex mt-10 lg:mt-3 gap-3">
           <FaTwitter />
           <FaFacebook />
           <FaInstagram />
